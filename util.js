d8b43153abbf88e8e85ef51b1f93e1dde3aa8532
Vaughn Kottler <Vaughn Kottler>
2016-08-11 07:39:43 -0700

    Really good progress on animated slideshow.

--- util.js
+++ util.js
@@ -3,9 +3,25 @@ var images = ["img/hack_illi.jpg", "img/presentation1.jpg", "img/presentation2.j
 var currImageIndex = 0;
 var currImage = images[currImageIndex];
 
+// cool function that handles transition
 function updateImage(galleryId, direction) {
+    var element = document.getElementById(galleryId);
+    var behind = galleryId + "_behind";
     currImageIndex += direction;
     if (currImageIndex == 0) currImageIndex = images.length - 1;
     if (currImageIndex == images.length) currImageIndex = 0;
-    document.getElementById(galleryId).style.backgroundImage = "url(" + images[currImageIndex] + ")";
+
+    element.className = "";
+    document.getElementById(behind).style.backgroundImage = "url(" + images[currImageIndex] + ")";
+    element.className = "transitionClear";
+
+    setTimeout(function() {
+        element.style.opacity = "0";
+        element.style.backgroundImage = "url(" + images[currImageIndex] + ")";
+    }, 500);
+
+    setTimeout(function() {
+        element.className = "transitionBack";
+        element.style.opacity = "1";
+    }, 750);
 }
