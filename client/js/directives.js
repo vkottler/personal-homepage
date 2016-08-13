364882239683c30e49e9ad1d7132c76ceb153099
Vaughn Kottler <vkottler@wisc.edu>
2016-08-13 12:50:43 -0500

    tons of progress!

--- client/js/directives.js
+++ client/js/directives.js
@@ -1,7 +1,13 @@
 angular.module('directives')
 
-.directive('vaughnToolbar', function () {
+.directive('vaughnToolbar', function() {
     return {
         templateUrl: 'directives/toolbar.html'
+    }
+})
+
+.directive('vaughnFooter', function() {
+    return {
+        templateUrl: 'directives/footer.html'
     }
 });
