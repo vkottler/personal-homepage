09396d185ef9e80c74e010d8f935ed45327fe0b7
vkottler <vkottler@wisc.edu>
2017-10-22 00:35:50 -0500

    updating books and slideshow

--- client/controllers/gallery.js
+++ client/controllers/gallery.js
@@ -1,3 +1,48 @@
+var harvardQuote = 
+	"<span style=\"font-size:0.75em\">\"Our motivation to <b>learn</b> is profoundly affected by how we think about intelligence.\" - ".concat(
+	"<i>Dr. Carol Deck, Harvard Psychologist</i></span>");
+
+var hsQuote = 
+	"<span style=\"font-size:0.75em\">\"Every single new, creative thing that you've ever seen is just a mashup of ideas that came before it.".concat(
+	"\" - <i>Yevgeniy Brikman</i> (<a href=\"https://www.amazon.com/").concat(
+	"Hello-Startup-Programmers-Building-Technologies/dp/1491909900/").concat(
+	"ref=sr_1_1?s=books&ie=UTF8&qid=1471412787&sr=1-1&keywords=hello+startup\" target=\"_newtab\">hello, startup</a>)</span>");
+
+var simplicity = 
+	"\"One day I will find the right words, and they will be simple.\"".concat(
+	" - <i>Jack Kerouac</i> (<a href=\"https://www.amazon.com/Dharma-Bums-Jack-Kerouac/dp/0140042520\" ").concat(
+	"target=\"_newtab\">The Dharma Bums</a>)");
+
+var linus = 
+	"\"Talk is cheap. Show me the <a href=\"https://github.com/vkottler/personal-homepage\" ".concat(
+	"target=\"_newtab\"><b>code</b></a>.\" ― <i>Linus Torvalds</i>");
+
+var quotes = [
+	harvardQuote,
+	hsQuote,
+	linus,
+	"Technology is my passion . . .",
+	"<span style=\"font-size:0.95em\">understanding the gritty details is my obsession . . .</span>",
+	"solving real human problems is my motivation . . .",
+	"and a collective love for computing is my dream.",
+	simplicity
+];
+
+var images = [
+	"img/slideshow/panel.jpg",
+	"img/slideshow/hack_illi.jpg",
+	"img/slideshow/electrical_box.jpg",
+	"img/slideshow/presentation2.jpg",
+	"img/slideshow/bench.jpg",
+	"img/slideshow/breadboard.jpg"
+];
+
+var loopDuration = 6000;
+var currImageIndex = 0;
+var currQuoteIndex = 0;
+var currImage = images[currImageIndex];
+var loopingSlideshow;
+
 angular.module('controllers')
 
 .controller('galleryController', function($scope) {
@@ -5,50 +50,7 @@ angular.module('controllers')
     // we need to do this because the controller reloads 
     // when you switch away from it and switch back to it
     clearInterval(loopingSlideshow);
-
-    var loopDuration = 6000;
-    var currImageIndex = 0;
-    var currQuoteIndex = 0;
-
-    var harvardQuote = 
-        "<span style=\"font-size:0.75em\">\"Our motivation to <b>learn</b> is profoundly affected by how we think about intelligence.\" - ".concat(
-        "<i>Dr. Carol Deck, Harvard Psychologist</i></span>");
-
-    var hsQuote = 
-        "<span style=\"font-size:0.75em\">\"Every single new, creative thing that you've ever seen is just a mashup of ideas that came before it.".concat(
-        "\" - <i>Yevgeniy Brikman</i> (<a href=\"https://www.amazon.com/").concat(
-        "Hello-Startup-Programmers-Building-Technologies/dp/1491909900/").concat(
-        "ref=sr_1_1?s=books&ie=UTF8&qid=1471412787&sr=1-1&keywords=hello+startup\" target=\"_newtab\">hello, startup</a>)</span>");
-
-    var simplicity = 
-        "\"One day I will find the right words, and they will be simple.\"".concat(
-        " - <i>Jack Kerouac</i> (<a href=\"https://www.amazon.com/Dharma-Bums-Jack-Kerouac/dp/0140042520\" ").concat(
-        "target=\"_newtab\">The Dharma Bums</a>)");
-
-    var linus = 
-        "\"Talk is cheap. Show me the <a href=\"https://github.com/vkottler/personal-homepage\" ".concat(
-        "target=\"_newtab\"><b>code</b></a>.\" ― <i>Linus Torvalds</i>");
-
-    var quotes = [
-        harvardQuote,
-        hsQuote,
-        linus,
-        "Technology is my passion . . .",
-        "<span style=\"font-size:0.95em\">understanding the gritty details is my obsession . . .</span>",
-        "solving real human problems is my motivation . . .",
-        "and a collective love for computing is my dream.",
-        simplicity
-    ];
-
-    var images = [
-        "img/hack_illi.jpg",
-        "img/presentation1.jpg",
-        "img/presentation2.jpg",
-        "img/presentation3.jpg",
-        "img/presentation4.jpg"
-    ];
     
-    var currImage = images[currImageIndex];
     document.getElementById("slideshow-text").innerHTML = quotes[currQuoteIndex];
 
     // cool function that handles transition
@@ -95,5 +97,5 @@ angular.module('controllers')
     function startLoop() {
         $scope.updateImage('slideshow', 1);
     }
-    var loopingSlideshow = setInterval(startLoop, loopDuration);
+    loopingSlideshow = setInterval(startLoop, loopDuration);
 });
---
+++ client/js/book_declarations.js
@@ -0,0 +1,405 @@
+var cssCard = new Card("CSS Secrets", "Lea Verou", 
+new IconSet(["border_style", "border_color"]), "img/books/css.jpg", 
+"Better Solutions to Everyday Web Design Problems", "", function() {
+	window.open("https://www.amazon.com/CSS-Secrets-Solutions-Everyday-Problems/dp/1449372635/");
+});
+
+var gameCoding = new Card("Game Coding Complete", "Mike McShaffry and David Graham", 
+new IconSet(["videogame_asset"]), "img/books/gameCoding.jpg", 
+"Fourth Edition", "", function() { 
+	window.open("https://www.amazon.com/Game-Coding-Complete-Fourth-McShaffry/dp/1133776574/");
+});
+
+var multiplayerCard = new Card("Multiplayer Game Programming", "Josh Glazer and Sanjay Madhav", 
+new IconSet(["router"]), "img/books/multiplayer.jpg", 
+"Architecting Networked Games (Game Design)", "", function() {
+	window.open("https://www.amazon.com/Multiplayer-Game-Programming-Architecting-Networked/dp/0134034309/");
+});
+
+var gameEngine = new Card("Game Engine Architecture", "Jason Gregory", 
+new IconSet(["memory"]), "img/books/gameEngine.jpg", 
+"Second Edition", "", function() {
+	window.open("https://www.amazon.com/Engine-Architecture-Second-Jason-Gregory/dp/1466560010/");
+});
+
+var helloStartup = new Card("Hello, Startup", "Yevgeniy Brikman", 
+new IconSet(["business_center", "monetization_on"]), "img/books/hello.jpg", 
+"A programmer's guide to building products, technologies, and teams", "", function() {
+	window.open("https://www.amazon.com/Hello-Startup-Programmers-Building-Technologies/dp/1491909900/");
+});
+
+var springCard = new Card("Spring Essentials", "Shameer Kunjumohamed and Hamidreza Sattari", 
+new IconSet(["developer_board"]), "img/books/spring.jpg", 
+"Build mission-critical enterprise applications", "", function() {
+	window.open("https://www.packtpub.com/application-development/spring-essentials");
+});
+
+var nodeCard = new Card("Node.js Design Patterns", "Mario Casciaro", 
+new IconSet(["http"]), "img/books/node.jpg", 
+"Get the best out of node by mastering a series of patterns and techniques", "", function() {
+	window.open("https://www.packtpub.com/web-development/nodejs-design-patterns-second-edition");
+});
+
+var rabpCard = new Card("Rails Angular Postgres Bootstrap", "David Bryant Copeland", 
+new IconSet(["bug_report"]), "img/books/rapb.jpg", 
+"Powerful Effective Efficient Full-Stack Web Development", "", function() {
+	window.open("https://www.amazon.com/Rails-Angular-Postgres-Bootstrap-Development/dp/1680501267/");
+});
+
+var modernPHPCard = new Card("Modern PHP", "Josh Lockhart", 
+new IconSet(["code"]), "img/books/modernPHP.jpg", 
+"New Features and Good Practices", "", function() {
+	window.open("https://www.amazon.com/Modern-PHP-Features-Good-Practices/dp/1491905018/");
+});
+
+var sqlCard = new Card("SQL in 10 Minutes", "Ben Forta", 
+new IconSet(["dns"]), "img/books/sql10.jpg", 
+"Sams Teach Yourself", "", function() {
+	window.open("https://www.amazon.com/SQL-Minutes-Sams-Teach-Yourself/dp/0672336073/");
+});
+
+var PHPwebServices = new Card("PHP Web Services", "Lorna Jane Mitchell", 
+new IconSet(["extensions"]), "img/books/php_web_services.jpg", 
+"APIs for the Modern Web", "", function() {
+	window.open("https://www.amazon.com/PHP-Web-Services-APIs-Modern/dp/1491933097/");
+});
+
+var mechantronics = new Card("Embedded Computing", "Kevin Lynch, Nicholas Marchuk, and Matthew Elwin", 
+new IconSet(["build"]), "img/books/mechatronics.jpg", 
+"Mechatronics with the PIC32", "", function() {
+	window.open("https://www.amazon.com/Embedded-Computing-Mechatronics-PIC32-Microcontroller/dp/0124201652/");
+});
+
+var embeddedSystems = new Card("Making Embedded Systems", "Elecia White", 
+new IconSet(["usb"]), "img/books/embedded-systems.jpg", 
+"Design Patterns for Great Software", "", function() {
+	window.open("https://www.amazon.com/Making-Embedded-Systems-Patterns-Software/dp/1449302149/");
+});
+
+var makeMoreElectronics = new Card("Make: More Electronics", "Charles Platt", 
+new IconSet(["memory"]), "img/books/make.jpg", 
+"Logic Chips, Amplifiers, Sensors and more", "", function() {
+	window.open("https://www.amazon.com/Make-Electronics-Journey-Amplifiers-Randomicity/dp/1449344046/");
+});
+
+var CANopen = new Card("Embedded Networking", "Olaf Pfeiffer, Andrew Arye, and Christian Keydel", 
+new IconSet(["call_split"]), "img/books/canopen.jpg", 
+"with CAN and CANopen", "", function() {
+	window.open("https://www.amazon.com/Embedded-Networking-CANopen-Olaf-Pfeiffer/dp/0692740872/");
+});
+
+var exploringPIC32 = new Card("Programming 32-bit Microcontrollers in C", "Lucio Di Jasio", 
+new IconSet(["sd_storage"]), "img/books/pic32.jpg", 
+"Exploring the PIC32", "", function() {
+	window.open("https://www.amazon.com/Programming-32-bit-Microcontrollers-Exploring-Technology/dp/0750687096/");
+});
+
+var photon = new Card("Make: Getting Started with the Photon", "Simon Monk", 
+new IconSet(["wifi_lock"]), "img/books/photon.jpg", 
+"Making Things with the Affordable, Compact, Hackable WiFi Module", "", function() {
+	window.open("https://www.amazon.com/Getting-Started-Photon-Affordable-Hackable/dp/1457187019/");
+});
+
+var programmingPi = new Card("Programming the Raspberry Pi", "Simon Monk", 
+new IconSet(["keyboard"]), "img/books/programmingPi.jpg", 
+"Getting Started with Python", "", function() {
+	window.open("https://www.amazon.com/Programming-Raspberry-Pi-Second-Getting/dp/1259587401/");
+});
+
+var pygame = new Card("Making Games with Python & Pygame", "Al Sweigart", 
+new IconSet(["mouse"]), "img/books/pygame.png", 
+"A guide to programming with graphics, animations, and sound", "", function() {
+	window.open("https://www.amazon.com/Making-Games-Python-Pygame-Sweigart/dp/1469901730/");
+});
+
+var pythonCiphers = new Card("Hacking Secret Ciphers with Python", "Al Sweigart", 
+new IconSet(["https"]), "img/books/ciphers.png", 
+"A beginner's guide to cryptography", "", function() {
+	window.open("https://www.amazon.com/Hacking-Secret-Ciphers-Python-cryptography/dp/1482614375/");
+});
+
+var cppPrimer = new Card("C++ Primer", "Stanley B. Lippman, Josée Lajoie, Barbara Moo", 
+new IconSet(["code"]), "img/books/cpp.jpg", 
+"Fifth Edition", "", function() {
+	window.open("https://www.amazon.com/Primer-5th-Stanley-B-Lippman/dp/0321714113/");
+});
+
+var rpiUserGuide = new Card("Raspberry Pi User Guide", "Eben Upton and Gareth Halfacree", 
+new IconSet(["developer_mode"]), "img/books/piUserGuide.jpg", 
+"Third Edition", "", function() {
+	window.open("https://www.amazon.com/Raspberry-User-Guide-Eben-Upton/dp/1118921666/");
+});
+
+var programmingPatterns = new Card("Game Programming Patterns", "Robert Nystrom", 
+new IconSet(["code"]), "img/books/patterns.jpg", 
+"", "", function() {
+	window.open("https://www.amazon.com/Game-Programming-Patterns-Robert-Nystrom/dp/0990582906/");
+});
+
+var sensorNetworks = new Card("Building Wireless Sensor Networks", "Robert Faludi", 
+new IconSet(["bluetooth"]), "img/books/sensor_networks.jpg", 
+"with ZigBee, XBee, Arduino, and Processing", "", function() {
+	window.open("https://www.amazon.com/Building-Wireless-Sensor-Networks-Processing/dp/0596807732/");
+});
+
+var electEncyc = new Card("Encyclopedia of Electronic Components", "Charles Platt", 
+new IconSet(["power"]), "img/books/encyc.jpeg", 
+"Volume 1", "", function() {
+	window.open("https://www.amazon.com/Encyclopedia-Electronic-Components-Capacitors-Transistors/dp/1449333893/");
+});
+
+/********************************* 10/21/2017 ********************************/
+var practElecInv = new Card(
+	"Practical Electronics for Inventors",
+	"Paul Scherz",
+	new IconSet(["build"]),
+	"img/books/pract_elect.jpg",
+	"Fourth Edition",
+	"", function() {
+		window.open("https://www.amazon.com/gp/product/1259587541/");
+	}
+);
+
+var programmingFPGAs = new Card(
+	"Programming FPGAs: Getting Started with Verilog",
+	"Simon Monk",
+	new IconSet(["memory"]),
+	"img/books/getting_started_fpgas.jpg",
+	"1st Edition",
+	"", function() {
+		window.open("https://www.amazon.com/gp/product/125964376X/");
+	}
+);
+
+var usbComplete = new Card(
+	"USB Complete: The Developer's Guide",
+	"Jan Axelson",
+	new IconSet(["usb"]),
+	"img/books/usbComplete.jpg",
+	"5th Edition",
+	"", function() {
+		window.open("https://www.amazon.com/gp/product/1931448280/");
+	}
+);
+
+var hardwareHacker = new Card(
+	"The Hardware Hacker: Adventures in Making and Breaking Hardware",
+	"Andrew Bunnie Huang",
+	new IconSet(["build"]),
+	"img/books/hardwareHacker.jpg",
+	"1st Edition",
+	"", function() {
+		window.open("https://www.amazon.com/gp/product/159327758X/");
+	}
+);
+
+var deepLearning = new Card(
+	"Deep Learning (Adaptive Computation and Machine Learning series)",
+	"Ian Goodfellow",
+	new IconSet(["developer board"]),
+	"img/books/deepLearning.jpg",
+	"",
+	"", function() {
+		window.open("https://www.amazon.com/gp/product/0262035618/");
+	}
+);
+
+var algorithms = new Card(
+	"Algorithms",
+	"Robert Sedgewick, Kevin Wayne",
+	new IconSet(["developer board"]),
+	"img/books/algorithms.jpg",
+	"volume or content",
+	"", function() {
+		window.open("https://www.amazon.com/gp/product/032157351X/");
+	}
+);
+
+var kernelDev = new Card(
+	"Linux Kernal Development",
+	"Robert Love",
+	new IconSet(["developer mode"]),
+	"img/books/kernelDev.jpg",
+	"3rd Edition",
+	"", function() {
+		window.open("https://www.amazon.com/gp/product/0672329468/");
+	}
+);
+
+var linuxProg = new Card(
+	"The Linux Programming Interface",
+	"author",
+	new IconSet(["developer mode"]),
+	"img/books/linuxProg.jpg",
+	"A Linux and UNIX System Programming Handbook",
+	"", function() {
+		window.open("https://www.amazon.com/gp/product/1593272200/");
+	}
+);
+
+var operatingSystems = new Card(
+	"Operating Systems: Principles and Practice",
+	"Thomas Anderson and Michael Dahlin",
+	new IconSet(["screen lock portrait"]),
+	"img/books/operatingSystems.jpg",
+	"volume or content",
+	"", function() {
+		window.open("https://www.amazon.com/gp/product/0985673524/");
+	}
+);
+
+var eaglePCBs = new Card(
+	"Make Your own PCBs with EAGLE",
+	"Simon Monk",
+	new IconSet(["timeline"]),
+	"img/books/eaglePCBs.jpg",
+	"From Schematic Designs to Finished Boards",
+	"", function() {
+		window.open("https://www.amazon.com/gp/product/0071819258/");
+	}
+);
+
+var GNUmake = new Card(
+	"GNU Make Reference Manual",
+	"Richard M. Stallman",
+	new IconSet(["build"]),
+	"img/books/GNUmake.jpg",
+	"Version 4.1",
+	"", function() {
+		window.open("https://www.amazon.com/gp/product/9881443512/");
+	}
+);
+
+var GCC = new Card(
+	"An Introduction to GCC",
+	"Brian J. Gough and Richard M. Stallman",
+	new IconSet(["build"]),
+	"img/books/GCC.jpg",
+	"For the GNU Compilers GCC and G++",
+	"", function() {
+		window.open("https://www.amazon.com/gp/product/0954161793/");
+	}
+);
+
+var embeddedRTOS = new Card(
+	"Embedded and Real-Time Operating Systems",
+	"K.C. Wang",
+	new IconSet(["memory"]),
+	"img/books/embeddedRTOS.jpg",
+	"1st Edition",
+	"", function() {
+		window.open("https://www.amazon.com/gp/product/3319515160/");
+	}
+);
+
+var bashRef = new Card(
+	"Bash Pocket Reference: Help for Power Users and Sys Admins",
+	"Arnold Robbins",
+	new IconSet(["code"]),
+	"img/books/bashRef.jpg",
+	"2nd Edition",
+	"", function() {
+		window.open("https://www.amazon.com/gp/product/1491941596/");
+	}
+);
+
+var cortexM = new Card(
+	"The Designer's Guide to the Cortex-M Processor Family",
+	"Trevor Martin",
+	new IconSet(["memory"]),
+	"img/books/cortexM.jpg",
+	"2nd Edition",
+	"", function() {
+		window.open("https://www.amazon.com/gp/product/0081006292/");
+	}
+);
+
+var tcpGuide = new Card(
+	"The TCP/IP Guide: A Comprehensive, Illustrated Internet Protocols Reference",
+	"author",
+	new IconSet(["cloud queue"]),
+	"img/books/tcpGuide.jpg",
+	"1st Edition",
+	"", function() {
+		window.open("https://www.amazon.com/gp/product/159327047X/");
+	}
+);
+
+var powerElectronics = new Card(
+	"Fundamentals of Power Electronics",
+	"Robert W. Erickson",
+	new IconSet(["power"]),
+	"img/books/powerElectronics.jpg",
+	"1st Edition",
+	"", function() {
+		window.open("https://www.amazon.com/gp/product/1461576482/");
+	}
+);
+
+var realTimeCPP = new Card(
+	"Real-Time C++: Efficient Object-Oriented and Template Microcontroller Programming",
+	"Christopher Kormanyos",
+	new IconSet(["memory"]),
+	"img/books/realTimeCPP.jpg",
+	"2nd Edition",
+	"", function() {
+		window.open("https://www.amazon.com/gp/product/3662478099/");
+	}
+);
+
+var embeddedLinux = new Card(
+	"Linux for Embedded and Real-time Applications",
+	"Doug Abbot",
+	new IconSet(["memory"]),
+	"img/books/embeddedLinux.jpg",
+	"3rd Edition",
+	"", function() {
+		window.open("https://www.amazon.com/");
+	}
+);
+/*****************************************************************************/
+
+var hardwareGroup = {
+	title: "Hardware",
+	description: "Books useful for programming embedded targets and developing embedded systems.",
+	books: [
+		mechantronics, CANopen, embeddedSystems, 
+		electEncyc, exploringPIC32, practElecInv,
+		usbComplete, kernelDev, linuxProg, embeddedRTOS,
+		cortexM, tcpGuide, realTimeCPP, embeddedLinux
+	]
+};
+var hobbyistGroup = {
+	title: "Hobbyist",
+	description: "Some fun books for tinerking with Raspberry Pis and other hobbyist grade tools and hardware.",
+	books: [
+		helloStartup, photon, programmingPi,
+		rpiUserGuide, sensorNetworks, makeMoreElectronics,
+		programmingFPGAs, hardwareHacker, eaglePCBs
+	]
+};
+var webDevGroup = {
+	title: "Web Development",
+	description: "Great resources for any and all web development projects.",
+	books: [
+		cssCard, springCard, nodeCard, modernPHPCard,
+		PHPwebServices, sqlCard, rabpCard
+	]
+};
+var programmingGroup = {
+	title: "Programming",
+	description: "For learning new languages and having references handy.",
+	books: [
+		cppPrimer, programmingPatterns, deepLearning,
+		algorithms, pythonCiphers, operatingSystems,
+		GNUmake, GCC, bashRef
+	]
+};
+var videoGamesGroup = {
+	title: "Video Game Programming",
+	description: "Materials related to videogame development.",
+	books: [
+		gameCoding, multiplayerCard, gameEngine, pygame
+	]
+};
+
